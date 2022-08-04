ab1e15bc791b6a5f27a302ae9c8bfb74ef023b95
ranyery <ranyerysc@gmail.com>
2022-08-04 11:20:40 -0300

    feat: cria entitidade de usuario e adiciona tipagem na service e controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 
 import { UsersService } from './users.services';
+import { User } from './user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  public create(@Body() user) {
+  public create(@Body() user: User): User {
     const createdUser = this.usersService.create(user);
     return createdUser;
   }
--- src/users/users.services.ts
+++ src/users/users.services.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
   private readonly users = [];
 
-  public create(user) {
+  public create(user: User): User {
     this.users.push(user);
     return user;
   }
