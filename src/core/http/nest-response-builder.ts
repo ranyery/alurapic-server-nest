646bc75298c5f4e758e8f5f7c743677ddef9345e
ranyery <ranyerysc@gmail.com>
2022-08-11 12:03:17 -0300

    feat: adiciona builder para customizar objeto de resposta

---
+++ src/core/http/nest-response-builder.ts
@@ -0,0 +1,28 @@
+import { NestResponse } from './nest-response';
+
+export class NestResponseBuilder {
+  private response: NestResponse = {
+    status: 200,
+    headers: {},
+    body: {},
+  };
+
+  public setStatus(status: number) {
+    this.response.status = status;
+    return this;
+  }
+
+  public setHeaders(headers: object) {
+    this.response.headers = headers;
+    return this;
+  }
+
+  public setBody(body: object) {
+    this.response.body = body;
+    return this;
+  }
+
+  public build() {
+    return new NestResponse(this.response);
+  }
+}
